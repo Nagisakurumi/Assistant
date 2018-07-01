b26e4cfd12e20b476a6b6288b01b715a6e0e8ff7
wangjiaxiaoxi <1767941752@qq.com>
2018-07-01 20:06:34 +0800

    项目创建代码

---
+++ Assistant/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assistant
+{
+    class Program
+    {
+
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            ConsoleHelper.hideConsole();
+            
+            ConsoleHelper.showConsole();
+            Console.ReadKey();
+        }
+
+        
+
+
+    }
+}
