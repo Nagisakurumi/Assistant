77802e379f2a75bd4f6997de4f9d643802714a19
NoWaring <786337054@qq.com>
2018-07-03 23:18:11 +0800

    添加 控制台输出图片

--- SmartQQTest/Program.cs
+++ SmartQQTest/Program.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using SmartQQ;
+using System.Drawing;
 
 namespace SmartQQTest
 {
@@ -44,7 +45,9 @@ namespace SmartQQTest
             Thread.Sleep(20);
             smartQQBot.Login();
             Thread.Sleep(20);
-            ShowImage(smartQQBot.SaveLoginImagePath);
+            //ShowImage(smartQQBot.SaveLoginImagePath);
+
+            ConsoleWriteImage(new Bitmap(smartQQBot.SaveLoginImagePath));
             while (true)
             {
                 Thread.Sleep(5000);
@@ -63,6 +66,185 @@ namespace SmartQQTest
             //string v =  regex.Match(value).Groups[1].Value;
             //Console.WriteLine(v);
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 在控制台绘制图片
+        /// </summary>
+        /// <param name="source"></param>
+        public static void ConsoleWriteImage(Bitmap source)
+        {
+            Bitmap bmp = (Bitmap)source;
+
+            int w = bmp.Width;
+            int h = bmp.Height;
+
+            int max = 28;
+
+            // we need to scale down high resolution images...
+            int complexity = (int)Math.Floor(Convert.ToDecimal(((w / max) + (h / max)) / 2));
+
+            if (complexity < 1) { complexity = 1; }
+
+            for (var x = 0; x < w; x += complexity)
+            {
+                for (var y = 0; y < h; y += complexity)
+                {
+                    Color clr = bmp.GetPixel(x, y);
+                    Console.ForegroundColor = getNearestConsoleColor(clr);
+                    Console.Write("█");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+        private static ConsoleColor getNearestConsoleColor(Color color)
+        {
+            // this is very likely to be awful and hilarious
+            int r = color.R;
+            int g = color.G;
+            int b = color.B;
+            int total = r + g + b;
+            decimal darkThreshold = 0.35m; // how dark a color has to be overall to be the dark version of a color
+
+            ConsoleColor cons = ConsoleColor.White;
+
+
+            if (total >= 39 && total < 100 && areClose(r, g) && areClose(g, b) && areClose(r, b))
+            {
+                cons = ConsoleColor.DarkGray;
+            }
+
+            if (total >= 100 && total < 180 && areClose(r, g) && areClose(g, b) && areClose(r, b))
+            {
+                cons = ConsoleColor.Gray;
+            }
+
+
+            // if green is the highest value
+            if (g > b && g > r)
+            {
+                // ..and color is less that 25% of color
+                if (Convert.ToDecimal(total / 765m) < darkThreshold)
+                {
+                    cons = ConsoleColor.DarkGreen;
+                }
+                else
+                {
+                    cons = ConsoleColor.Green;
+                }
+            }
+
+            // if red is the highest value
+            if (r > g && r > b)
+            {
+
+                // ..and color is less that 25% of color
+                if (Convert.ToDecimal(total / 765m) < darkThreshold)
+                {
+                    cons = ConsoleColor.DarkRed;
+                }
+                else
+                {
+                    cons = ConsoleColor.Red;
+                }
+            }
+
+            // if blue is the highest value
+            if (b > g && b > r)
+            {
+                // ..and color is less that 25% of color
+                if (Convert.ToDecimal(total / 765m) < darkThreshold)
+                {
+                    cons = ConsoleColor.DarkBlue;
+                }
+                else
+                {
+                    cons = ConsoleColor.Blue;
+                }
+            }
+
+
+            if (r > b && g > b && areClose(r, g))
+            {
+                // ..and color is less that 25% of color
+                if (Convert.ToDecimal(total / 765m) < darkThreshold)
+                {
+                    cons = ConsoleColor.DarkYellow;
+                }
+                else
+                {
+                    cons = ConsoleColor.Yellow;
+                }
+            }
+
+
+
+            if (b > r && g > r && areClose(b, g))
+            {
+                // ..and color is less that 25% of color
+                if (Convert.ToDecimal(total / 765m) < darkThreshold)
+                {
+                    cons = ConsoleColor.DarkCyan;
+                }
+                else
+                {
+                    cons = ConsoleColor.Cyan;
+                }
+            }
+
+
+
+
+
+            if (r > g && b > g && areClose(r, b))
+            {
+                // ..and color is less that 25% of color
+                if (Convert.ToDecimal(total / 765m) < darkThreshold)
+                {
+                    cons = ConsoleColor.DarkMagenta;
+                }
+                else
+                {
+                    cons = ConsoleColor.Magenta;
+                }
+            }
+
+            if (total >= 180 && areClose(r, g) && areClose(g, b) && areClose(r, b))
+            {
+                cons = ConsoleColor.White;
+            }
+
+
+            // BLACK
+            if (total < 39)
+            {
+                cons = ConsoleColor.Black;
+            }
+
+
+
+
+
+            return cons;
+        }
+        /// <summary>
+        /// Returns true if the numbers are pretty close
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static bool areClose(int a, int b)
+        {
+            int diff = Math.Abs(a - b);
+
+            if (diff < 30)
+            {
+                return true;
+            }
+            else return false;
+
         }
     }
 }
